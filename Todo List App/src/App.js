a0cc3505c7bfe4d611f3e643d58f3c9a71c29b1a
Moksh45 <mokshgupta567@gmail.com>
2023-06-14 00:29:23 +0530

    added add items functionality in todolist

--- Todo List App/src/App.js
+++ Todo List App/src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
 
 const App = () => {
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  };
+
+  let arr = []
+  const listofItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
   return (
     <>
       <div className='main_div'>
@@ -9,12 +24,16 @@ const App = () => {
           <br />
           <h1>ToDo List</h1>
           <br />
-          <input type="text" placeholder='Add a items'/>
-          <button>
+          <input type="text" placeholder='Add a items'
+            onChange={itemEvent} value={inputList}
+          />
+          <button onClick={listofItems}>
             +
           </button>
           <ol>
-            <li> buy apple</li>
+            {Items.map((itemval) => {
+              return <ToDoList text={itemval}/>
+            })}
           </ol>
         </div>
       </div>
